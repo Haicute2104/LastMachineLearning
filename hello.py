668009895e67051583b68988846ee67a5d614e1b
Haicute2104 <haicute2104@gmail.com>
2024-10-30 23:43:08 +0700

    LastEnd

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import Lasso
 from sklearn.neural_network import MLPRegressor 
 from sklearn.ensemble import StackingRegressor
